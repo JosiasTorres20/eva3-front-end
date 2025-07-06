5dde0ae646b76e47ebbc3ddf8dc96d1f664503c8
JosiasTorres20 <jositoerr6776@gmail.com>
2025-07-05 21:46:22 -0400

    Llamo para ejecutar los componentes de Formulario y Tabla
    Llame ambos componentes y les pase las propiedades necesarios para que se puedan ejecutar correctamente.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import {Proyecto} from "./interfaces/Proyecto"
+import Formulario from "./components/Formulario";
+import Tabla from "./components/Tabla";
 
 
 const initialStateProyecto: Proyecto = {
@@ -14,6 +16,24 @@ export default function Principal(){
 
   const [proyecto, setProyecto] = useState<Proyecto>(initialStateProyecto);
   const [proyectos, setProyectos]= useState<Proyecto[]>([]);
+
+
+  return (
+    <main>
+      <Formulario
+        proyecto={initialStateProyecto}
+        handleProyecto={() => {}}
+        handleRegistrar={() => {}}
+        enEdicion={false}
+      />
+
+      <Tabla
+        proyectos={proyectos}
+        getProyecto={() =>{}}
+        eliminarProyecto={() => {}}
+      />
+    </main>
+  )
 
   
 }
