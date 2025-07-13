c638221ba8db75b53e1ec2e2e853243a6985958e
JosiasTorres20 <jositoerr6776@gmail.com>
2025-07-13 15:41:26 -0400

    cargo el archivo de configuracion "firebase.ts"
    inserto el código proporcionado por firebase para poder conectar mi APP con la base de datos

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDgQuFAvfSR7kToEwVVCiBbyhHU5lzPik",
+  authDomain: "eva4-f2fbb.firebaseapp.com",
+  projectId: "eva4-f2fbb",
+  storageBucket: "eva4-f2fbb.firebasestorage.app",
+  messagingSenderId: "355667945244",
+  appId: "1:355667945244:web:b996fd17a1787a32acbcd9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
