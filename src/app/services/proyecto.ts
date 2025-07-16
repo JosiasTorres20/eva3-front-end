b654e702c3cdcb4609a3fd132c9ae489d375ba41
JosiasTorres20 <jositoerr6776@gmail.com>
2025-07-16 16:23:53 -0400

    creo la funcion crearProyecto con async para que espere correctamente la respuesta de firebase
    importo las funciones (addDoc y collection) desde (firebase/firestore) y la instancia (db) desde (firebase.ts) para conectarme a la base de datos.
    
    cree la funcioln (crearProyecto) dentro de (services/proyecto.ts) para desarrollar la logica de creacion de documentos en la coleccion (proyectos).
    
    intente manejar errores esperando códigos de HTTP (por ejemplo, 400, 403 o 500), ya que había leído que esos codigos se usan cuando hay errores en solicitudes web. Pero al trabajar con el SDK me di cuenta de que Firebase devuelve directamente esos codigos ya que funciona con promesas.Por eso tuve por usar (async/await) para poder capturar los errores con try/catch ya que es distinto a cuando se usaba fetch donde si se pueden revisar los codigos de estado.

--- src/app/services/proyecto.ts
+++ src/app/services/proyecto.ts
@@ -1,2 +1,14 @@
 import {db} from "../../firebase";
+import {addDoc, collection} from "firebase/firestore";
+import {Proyecto} from "../interfaces/Proyecto";
 
+
+
+export const crearProyecto = async (proyecto:Proyecto): Promise<string> => {
+    try{
+        const nuevoDocumento = await addDoc(collection(db, "proyectos"), proyecto);
+        return nuevoDocumento.id
+    }catch (error){
+        throw error;
+    }
+}
