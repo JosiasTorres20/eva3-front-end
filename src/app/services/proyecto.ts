a3d6d2f5d15456a8ea5fe36deeb213874a27d120
JosiasTorres20 <jositoerr6776@gmail.com>
2025-07-17 10:36:27 -0400

    agrego funciones faltantes del CRUD para gestionar los proyectos en Firebase
    importo funciones de Firebase (addDoc, getDocs, updateDoc, deleteDoc y doc) y la instancia (db) desde (firebase.ts) para poder usar los datos dentro de la coleccion "proyectos".
    
    cree las siguientes funciones:
    - verProyecto: busca todos los documentos de la colecciion y los deserializa transformándolo en objetos de proyecto con su id.
    - actualizarProyecto: recibe un id desde el (firebase) y los datos actualizados despues actualiza el documento en Firebase.
    - eliminarProyecto: busca el proyecto por su id y lo elimina de la bd.

--- src/app/services/proyecto.ts
+++ src/app/services/proyecto.ts
@@ -1,5 +1,5 @@
 import {db} from "../../firebase";
-import {addDoc, collection} from "firebase/firestore";
+import {addDoc, collection, getDocs, updateDoc, deleteDoc, doc} from "firebase/firestore";
 import {Proyecto} from "../interfaces/Proyecto";
 
 
@@ -12,3 +12,37 @@ export const crearProyecto = async (proyecto:Proyecto): Promise<string> => {
         throw error;
     }
 }
+
+export const verProyecto = async ()=> {
+    const datos = await getDocs(collection(db, "proyectos"));
+    const proyectos = datos.docs.map(doc => {
+        const informacion = doc.data();
+        return {
+            id: doc.id,
+            nombre: informacion.nombre,
+            presupuesto: informacion.presupuesto,
+            tipo: informacion.tipo,
+            objetivo: informacion.objetivo,
+            fecha: informacion.fecha
+        };
+    });
+    return proyectos;
+}
+
+export const actualizarProyecto = async (proyectoId:string, datosActualizados:Proyecto) => {
+    const proyectos = doc(db, "proyectos", proyectoId);
+    await updateDoc(proyectos, {
+        nombre: datosActualizados.nombre,
+        presupuesto: datosActualizados.presupuesto,
+        tipo: datosActualizados.tipo,
+        objetivo: datosActualizados.objetivo,
+        fecha: datosActualizados.fecha
+    })
+}
+
+export const eliminarProyecto = async (proyectoId: string) => {
+    const proyectos = doc(db, "proyectos", proyectoId);
+    await deleteDoc(proyectos)
+}
+
+
